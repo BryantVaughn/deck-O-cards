e303b9ee1bd1d8ab9520a93c325ab7ff8a258c9b
Bryant Vaughn <bf_vaughn@hotmail.com>
2020-03-17 18:44:17 -0600

    initial component structure

---
+++ src/Deck.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Deck extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawnCards = []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Deck;
